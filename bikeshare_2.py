ad2640c4c014d935c3fb3252000b961626de9994
Susana Chicano <schicano@gmail.com>
2020-11-24 20:32:01 -0800

    Additional comments

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -8,7 +8,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Asks user to specify a city, month, and day and provides analysis based on those choices.
 
     Returns:
         (str) city - name of the city to analyze
